104ed200c6354a1cd40fc9a79dc2106cd3f2a730
tonyji <jim.tony@navis.com>
2022-12-06 00:44:01 +0530

    First Spring App

---
+++ Spring Sample Demo/src/com/coach/springdemo/SpringHelloApp.java
@@ -0,0 +1,24 @@
+package com.coach.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringHelloApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Create Spring Container
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//Retrieve Bean
+		Coach c1 = context.getBean("myCoach",Coach.class);
+		
+		//Call methods on the Bean
+		System.out.println(c1.getDailyWorkout());
+		
+		
+		//Closing the context
+		context.close();
+	}
+
+}
