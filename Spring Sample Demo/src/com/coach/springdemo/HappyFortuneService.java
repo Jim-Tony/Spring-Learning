885e45ec2502c56a9139f4e0b4ff7595e8a5aa02
tonyji <jim.tony@navis.com>
2022-12-17 16:08:16 +0530

    logic implementation

--- Spring Sample Demo/src/com/coach/springdemo/HappyFortuneService.java
+++ Spring Sample Demo/src/com/coach/springdemo/HappyFortuneService.java
@@ -5,7 +5,13 @@ public class HappyFortuneService implements FortuneService {
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return "Have a lucky day today Done!!!";
+		String[] fortunes = {"Have a lucky day today!!!",
+				"Be carefull today",
+				"Enjoy the day"};
+		int max = fortunes.length-1;
+		int min = 0;
+		int random = (int) Math.floor(Math.random()*(max-min+1)+min);
+		return fortunes[random];
 	}
 
 }
