17916bf4839d8a382a9ebe0f4869df7f47ca4905
tonyji <jim.tony@navis.com>
2023-02-05 21:54:14 +0530

    Spring MVC demo initial

---
+++ spring-mvc-demo/WebContent/WEB-INF/view/show-form.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Form</title>
+</head>
+<body>
+<form action="processFormVersionTwo" method="GET">
+	<label for="name">Enter the name</label>
+	<input id="name" name="myName" type="text">
+	<input type="submit">
+</form>
+</body>
+</html>
---
+++ spring-mvc-demo/src/com/tony/javamvc/HomeController.java
@@ -0,0 +1,34 @@
+package com.tony.javamvc;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String showHomePage() {
+		return "main-menu";
+	}
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "show-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "process-form";
+	}
+	
+	//Adding data to model
+	@RequestMapping("/processFormVersionTwo")
+	public String convertToUpperCase(HttpServletRequest request, Model model) {
+		String theName = request.getParameter("myName");
+		theName = "Hey "+theName.toUpperCase();
+		model.addAttribute("name",theName);
+		return "process-form";
+	}
+}
